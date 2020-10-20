a2ffcb59bbe0fa4cd432aae2715b3fed6ba1847e
Jacob Boodry <jake.boodry@live.com>
2020-10-20 13:01:36 -0700

    updated sort function

--- api.js
+++ api.js
@@ -10,32 +10,23 @@ app.get('/', (req, res) =>{
 })
 
 app.get('/apps', (req, res) => {
+  console.log('req.query ', req.query)
   const {genres, sort} = req.query;
   let data = games;
 
   if (sort) {
-    if (!['rating', 'app'].includes(sort)){
+    if (!['Rating', 'App'].includes(sort)){
       return res.status(400).send('Sort must be Rating or App.')
     }
   }
+console.log(sort);
 
   if (sort) {
     data.sort((currentGame, nextGame) => {
-      // if (currentGame[sort] > nextGame[sort]){
-      //   return 1
-      // }
-      // else if (currentGame[sort] < nextGame[sort]){
-      //   return -1
-      // }
-      // else {
-      //   return 0
-      // }
-
       return currentGame[sort] > nextGame[sort] ? 1 : currentGame[sort] < nextGame[sort] ? -1 : 0;
     })
   }
-   
-  res.send(data);
+  res.status(200).send(data);
 })
 
 app.listen(8000, () => {
