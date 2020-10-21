d739c861e0d21e20626e0d2153db2cd0fa5d992e
ashen-develops <ashenp.designs@gmail.com>
2020-10-21 10:35:00 -0500

    init

--- api.js
+++ api.js
@@ -23,12 +23,15 @@ console.log(sort);
 
   if (sort) {
     data.sort((currentGame, nextGame) => {
-      return currentGame[sort] > nextGame[sort] ? 1 : currentGame[sort] < nextGame[sort] ? -1 : 0;
-    })
+      // return currentGame[sort] > nextGame[sort] ? 1 : currentGame[sort] < nextGame[sort] ? -1 : 0;
+      if (currentGame[sort] > nextGame[sort]) return 1;
+      if (currentGame[sort] < nextGame[sort]) return -1;
+      return 0;
+    });
   }
   res.status(200).send(data);
-})
+});
 
 app.listen(8000, () => {
-  console.log('Server started on port 8000')
-})
+  console.log('Server started on port 8000');
+});
